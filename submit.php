3bbe5278b5e317f3b488351b08e1ae00a8bfc1ee
Student-Ayanpal <ayanp0759@gmail.com>
2025-12-25 07:44:54 +0530

    Added server-side validation in submit.php

--- submit.php
+++ submit.php
@@ -4,10 +4,29 @@ $name=$_POST['name'];
 $email=$_POST['email'];
 $message=$_POST['message']; 
 
-echo "<h2>Form Submitted Successfully</h2>";
-echo "<p><strong>Name:</strong> $name</p>";
-echo "<p><strong>Email:</strong> $email</p>";
-echo "<p><strong>Message:</strong> $message</p>";
+if(empty($name)){
+    die("Name is required");
+}
+
+if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+    die("Valid email is required");
+}
+
+if(empty($message)){
+    die("Message is required");
+}
+
+$conn=mysqli_connect("localhost","root","","contact_db");
+if(!$conn){
+    die("Connection failed: ");
+}
+
+$sql = "INSERT INTO MESSAGES(NAME,EMAIL,MESSAGE) VALUES ('$name','$email','$message')";
+
+if(mysqli_query($conn,$sql)){
+    echo "<h2>Message saved successfully!</h2>";
+}
+mysqli_close($conn);
 
 
 ?>
